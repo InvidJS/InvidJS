8dc3a78cf4d907ca6ea303d4dd545bab3ee1d245
Inspector Soviet <www.alex.avil@mail.ru>
2023-03-03 23:44:41 +0400

    constants class

--- classes/index.js
+++ classes/index.js
@@ -1,4 +1,5 @@
 import { Video } from "../classes/video.js";
 import { Instance } from "../classes/instance.js";
 import { Playlist } from "../classes/playlist.js";
-export { Video, Instance, Playlist };
+import { Constants } from "../classes/constants.js";
+export { Video, Instance, Playlist, Constants };
