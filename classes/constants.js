fb3f8a33da8b7f68293d1e575ba9852df53bcc61
Inspector Soviet <www.alex.avil@mail.ru>
2023-03-04 23:57:37 +0400

    restrict allowed audio quality

--- classes/constants.js
+++ classes/constants.js
@@ -1,4 +1,8 @@
 export let Constants = {
-    allowedTypes:
-     ["https", "i2p", "onion", "all"]
-}
+  allowedTypes: ["https", "i2p", "onion", "all"],
+  allowedAudioQuality: {
+    "AUDIO_QUALITY_ULTRALOW": 0,
+    "AUDIO_QUALITY_LOW": 1,
+    "AUDIO_QUALITY_MEDIUM": 2,
+  },
+};
