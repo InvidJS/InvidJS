99509c234f117a53f76ece50bda2ad35bb7d092a
Inspector Soviet <www.alex.avil@mail.ru>
2023-03-18 16:19:33 +0400

    swap test video

--- tests/video.test.ts
+++ tests/video.test.ts
@@ -4,28 +4,28 @@ import * as InvidJS from "../index";
 describe("Video fetch test", () => {
     test("Video must be fetched correctly.", async () => {
         let instance = await InvidJS.fetchInstances({url: "https://y.com.sb"});
-        expect(await InvidJS.fetchVideo(instance[0], "DleL5C130DU")).not.toBeNull();
+        expect(await InvidJS.fetchVideo(instance[0], "dQw4w9WgXcQ")).not.toBeNull();
     })
 
     test("Must be able to fetch minimal video.", async () => {
         let instance = await InvidJS.fetchInstances({url: "https://y.com.sb"});
-        expect((await InvidJS.fetchVideo(instance[0], "DleL5C130DU", {type: FetchTypes.Minimal})).id).not.toBe(undefined);
+        expect((await InvidJS.fetchVideo(instance[0], "dQw4w9WgXcQ", {type: FetchTypes.Minimal})).id).not.toBe(undefined);
     })
 
     test("Must be able to fetch basic video.", async () => {
         let instance = await InvidJS.fetchInstances({url: "https://y.com.sb"});
-        expect((await InvidJS.fetchVideo(instance[0], "DleL5C130DU", {type: FetchTypes.Basic})).formats).not.toBe(undefined);
+        expect((await InvidJS.fetchVideo(instance[0], "dQw4w9WgXcQ", {type: FetchTypes.Basic})).formats).not.toBe(undefined);
     })
 
     test("Must be able to fetch full video.", async () => {
         let instance = await InvidJS.fetchInstances({url: "https://y.com.sb"});
-        expect((await InvidJS.fetchVideo(instance[0], "DleL5C130DU", {type: FetchTypes.Full})).description).not.toBe(undefined);
+        expect((await InvidJS.fetchVideo(instance[0], "dQw4w9WgXcQ", {type: FetchTypes.Full})).description).not.toBe(undefined);
     })
 
     test("Must throw an error if API is blocked.", async () => {
         let instance = await InvidJS.fetchInstances({url: "https://yewtu.be"});
         try {
-            await InvidJS.fetchVideo(instance[0], "DleL5C130DU");
+            await InvidJS.fetchVideo(instance[0], "dQw4w9WgXcQ");
         } catch (error: any) {
             expect(error.message).toBe("The instance you provided does not support API requests or is offline!")
         }
